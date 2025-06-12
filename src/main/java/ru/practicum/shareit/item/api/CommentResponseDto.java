86aaeff848541288d8c6147c8cbfbc8b4767173d
Dmitriy Gorokhov <dgorokhov123@gmail.com>
2025-06-12 20:17:44 +0300

    feat: Item Entity and JPA Repository changes

---
+++ src/main/java/ru/practicum/shareit/item/api/CommentResponseDto.java
@@ -0,0 +1,33 @@
+package ru.practicum.shareit.item.api;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import ru.practicum.shareit.item.Comment;
+import ru.practicum.shareit.serializer.OffsetDateTimeDeserializer;
+import ru.practicum.shareit.serializer.OffsetDateTimeSerializer;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class CommentResponseDto {
+
+    Long id;
+    String text;
+    String authorName;
+
+    @JsonSerialize(using = OffsetDateTimeSerializer.class)
+    @JsonDeserialize(using = OffsetDateTimeDeserializer.class)
+    OffsetDateTime created;
+
+    public static CommentResponseDto from(Comment comment) {
+        if (comment == null) return null;
+        CommentResponseDto dto = new CommentResponseDto();
+        dto.setId(comment.getId());
+        dto.setText(comment.getText());
+        dto.setAuthorName(comment.getAuthor().getName());
+        dto.setCreated(comment.getCreated());
+        return dto;
+    }
+
+}
